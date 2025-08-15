bb81902c2efa9bb0d94c225bfa1adb745886103a
dabilci <dabilci@users.noreply.github.com>
2025-08-15 12:55:36 +0000

    deploy: 82dd94b1b3654ff2ded220a332f5accd32b91543

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "38ebe7ed30e9b094a6f6bda700a81ad4",
+const RESOURCES = {"flutter_bootstrap.js": "fae495de555c1a0cdcc9a69a5a3b6e64",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
